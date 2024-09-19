dbb087b97f842864f77ab288078e97a58b81a3b5
AdamZed <adamzanon@hotmail.com>
2024-09-19 14:42:07 -0400

    dasiyui init

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,24 @@ export default {
 	theme: {
 		extend: {}
 	},
-	plugins: []
+
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#38bdf8',
+					secondary: '#c084fc',
+					accent: '#3b82f6',
+					neutral: '#e7e5e4',
+					'base-100': '#1f2937',
+					info: '#f9a8d4',
+					success: '#4ade80',
+					warning: '#fcd34d',
+					error: '#ef4444'
+				}
+			}
+		]
+	},
+
+	plugins: [require('daisyui')]
 };
