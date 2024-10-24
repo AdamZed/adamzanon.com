2c0f71354f4418a4ed102974c9f08f6982234ef3
AdamZed <adamzanon@hotmail.com>
2024-10-24 12:31:37 -0400

    upgrade to svelte 5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,11 +9,11 @@
 <div class="mx-10 mt-20 border-t-2 border-secondary pt-2 lg:mx-52 lg:mt-48 lg:pt-6">
 	<h1 class="font-mono text-3xl font-bold text-primary">Adam Zanon</h1>
 	<div>
-		<a target="_blank" href="https://www.github.com/AdamZed">
-			<span class="icon-[mingcute--github-line] size-7 text-neutral hover:text-secondary" />
+		<a aria-label="github link" target="_blank" href="https://www.github.com/AdamZed">
+			<span class="icon-[mingcute--github-line] size-7 text-neutral hover:text-secondary"></span>
 		</a>
-		<a target="_blank" href="https://www.linkedin.com/in/adam-zanon/">
-			<span class="icon-[mingcute--linkedin-line] size-7 text-neutral hover:text-secondary" />
+		<a aria-label="linkedin link" target="_blank" href="https://www.linkedin.com/in/adam-zanon/">
+			<span class="icon-[mingcute--linkedin-line] size-7 text-neutral hover:text-secondary"></span>
 		</a>
 	</div>
 </div>
@@ -21,7 +21,12 @@
 <Projects />
 
 <div class="footer mb-2 flex justify-center text-center text-neutral">
-	© 2024 Adam Zanon<a target="_blank" href="https://www.github.com/AdamZed/adamzanon.com">
-		<span class="icon-[mingcute--github-fill] size-5 text-neutral hover:text-secondary" />
+	© 2024 Adam Zanon
+	<a
+		aria-label="project source link"
+		target="_blank"
+		href="https://www.github.com/AdamZed/adamzanon.com"
+	>
+		<span class="icon-[mingcute--github-fill] size-5 text-neutral hover:text-secondary"></span>
 	</a>
 </div>
