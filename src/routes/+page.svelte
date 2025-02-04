03420ca0531136d4262b534b3e82b95c4e5236ba
AdamZed <adamzanon@hotmail.com>
2025-02-04 11:47:34 -0500

    Add Strava Link, 2025 C

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,11 +9,29 @@
 <div class="mx-10 mt-20 border-t-2 border-secondary pt-2 lg:mx-52 lg:mt-48 lg:pt-6">
 	<h1 class="font-mono text-3xl font-bold text-primary">Adam Zanon</h1>
 	<div>
-		<a aria-label="github link" target="_blank" href="https://www.github.com/AdamZed">
+		<a
+			aria-label="github link"
+			title="Github"
+			target="_blank"
+			href="https://www.github.com/AdamZed"
+		>
 			<span class="icon-[mingcute--github-line] size-7 text-neutral hover:text-secondary"></span>
 		</a>
-		<a aria-label="linkedin link" target="_blank" href="https://www.linkedin.com/in/adam-zanon/">
+		<a
+			aria-label="linkedin link"
+			title="LinkedIn"
+			target="_blank"
+			href="https://www.linkedin.com/in/adam-zanon/"
+		>
 			<span class="icon-[mingcute--linkedin-line] size-7 text-neutral hover:text-secondary"></span>
+		</a>
+		<a
+			aria-label="strava link"
+			title="Strava"
+			target="_blank"
+			href="https://www.strava.com/athletes/61712410"
+		>
+			<span class="icon-[mingcute--run-line] size-7 text-neutral hover:text-secondary"></span>
 		</a>
 	</div>
 </div>
@@ -22,7 +40,7 @@
 
 <footer class="footer footer-center rounded-t p-10">
 	<p class="flex text-neutral">
-		© 2024 Adam Zanon <a
+		© 2024-2025 Adam Zanon <a
 			aria-label="project source link"
 			target="_blank"
 			href="https://www.github.com/AdamZed/adamzanon.com"
